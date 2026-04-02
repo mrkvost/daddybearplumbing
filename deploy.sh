50abb90dcf7dcd9a056a8b2280866f0d2331ec1e
Michal Hola <mrkvost@gmail.com>
2026-04-02 15:35:23 +0200

    cache headers

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -e
 
-echo "Uploading to S3..."
-aws s3 sync dist/daddy-bear-plumbing/browser/ s3://kvaking --delete
+DIST="dist/daddy-bear-plumbing/browser"
+BUCKET="s3://kvaking"
+
+echo "Uploading hashed assets (long cache)..."
+aws s3 sync "$DIST" "$BUCKET" --delete \
+  --exclude "index.html" \
+  --exclude "robots.txt" \
+  --exclude "sitemap.xml" \
+  --cache-control "public, max-age=31536000, immutable"
+
+echo "Uploading index.html, robots.txt, sitemap.xml (no cache)..."
+aws s3 cp "$DIST/index.html" "$BUCKET/index.html" --cache-control "no-cache"
+aws s3 cp "$DIST/robots.txt" "$BUCKET/robots.txt" --cache-control "no-cache"
+aws s3 cp "$DIST/sitemap.xml" "$BUCKET/sitemap.xml" --cache-control "no-cache"
 
 echo "Invalidating CloudFront cache..."
 aws cloudfront create-invalidation --distribution-id E13CC4GUW5MN29 --paths "/*"
